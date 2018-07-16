59ef8b7bd942f693d1306688ffe579ad64ac71a7
Jiddu Alexander <san@nesware.net>
2018-07-16 14:24:32 +0200

    elements roxygen comments to normal comments

--- R/elements.R
+++ R/elements.R
@@ -1,61 +1,61 @@
-#' 
-#' #' Labguru add element
-#' #'
-#' #' Add a new element to labguru
-#' #'
-#' #' @param name character(1) The name of the element
-#' #' @param experiment_id numeric(1) The experiment id for which to add a new procedure
-#' #' @param return character(1) whether the function returns either 'id' (default) or 'all' element information
-#' #' @param server character(1) indicating the server URL
-#' #' @param token character(1) access token for API authentication
-#' #'
-#' #' @return list with either element id only or all element information
-#' #' @export
-#' #'
-#' #' @import httr
-#' #' @importFrom jsonlite fromJSON
-#' #'
-#' #' @examples
-#' #' \dontrun{
-#' #' labguru_add_element(project_id  = 1,
-#' #'                     name        = "My new element",
-#' #'                     description = "This element contains ...")
-#' #' }
-#' labguru_add_element <- function(name,
-#'                                 experiment_id,
-#'                                 return      = "id",
-#'                                 server      = Sys.getenv("LABGURU_SERVER"),
-#'                                 token       = Sys.getenv("LABGURU_TOKEN")) {
-#'   
-#'   # Test arguments
-#'   
-#'   # CHECK ARG experiment ID
-#'   
-#'   stopifnot(is.character(return))
-#'   stopifnot(length(return) == 1)
-#'   stopifnot(return %in% c('id', 'all'))
-#'   
-#'   # URL
-#'   url <- httr::modify_url(url   = server,
-#'                           path  = "/api/v1/element_containers")
-#'   
-#'   # Body
-#'   body <- list("token"                = token,
-#'                "item[name]"           = name,
-#'                "item[container_id]"   = experiment_id,
-#'                "item[container_type]" = "Projects::Experiment")
-#'   
-#'   # POST
-#'   parsed <- labguru_post_item(url  = url,
-#'                               body = body)
-#'   
-#'   # return information
-#'   if (return == "id") {
-#'     list(id = parsed$id)
-#'   } else {
-#'     parsed
-#'   }
-#' }
+# 
+# #' Labguru add element
+# #'
+# #' Add a new element to labguru
+# #'
+# #' @param name character(1) The name of the element
+# #' @param experiment_id numeric(1) The experiment id for which to add a new procedure
+# #' @param return character(1) whether the function returns either 'id' (default) or 'all' element information
+# #' @param server character(1) indicating the server URL
+# #' @param token character(1) access token for API authentication
+# #'
+# #' @return list with either element id only or all element information
+# #' @export
+# #'
+# #' @import httr
+# #' @importFrom jsonlite fromJSON
+# #'
+# #' @examples
+# #' \dontrun{
+# #' labguru_add_element(project_id  = 1,
+# #'                     name        = "My new element",
+# #'                     description = "This element contains ...")
+# #' }
+# labguru_add_element <- function(name,
+#                                 experiment_id,
+#                                 return      = "id",
+#                                 server      = Sys.getenv("LABGURU_SERVER"),
+#                                 token       = Sys.getenv("LABGURU_TOKEN")) {
+#   
+#   # Test arguments
+#   
+#   # CHECK ARG experiment ID
+#   
+#   stopifnot(is.character(return))
+#   stopifnot(length(return) == 1)
+#   stopifnot(return %in% c('id', 'all'))
+#   
+#   # URL
+#   url <- httr::modify_url(url   = server,
+#                           path  = "/api/v1/element_containers")
+#   
+#   # Body
+#   body <- list("token"                = token,
+#                "item[name]"           = name,
+#                "item[container_id]"   = experiment_id,
+#                "item[container_type]" = "Projects::Experiment")
+#   
+#   # POST
+#   parsed <- labguru_post_item(url  = url,
+#                               body = body)
+#   
+#   # return information
+#   if (return == "id") {
+#     list(id = parsed$id)
+#   } else {
+#     parsed
+#   }
+# }
 
 #' Labguru list elements
 #' 
@@ -130,40 +130,36 @@ labguru_list_elements <- function(experiment_id = NULL,
 }
 
 
-#' #' Labguru get element
-#' #'
-#' #' Takes a element id and gets the element information.
-#' #'
-#' #' @param element_id numeric(1) id indicating a element on labguru server
-#' #' @param server character(1) indicating the server URL
-#' #' @param token character(1) access token for API authentication
-#' #'
-#' #' @return list object of labguru element
-#' #' @export
-#' #'
-#' #' @import httr
-#' #' @importFrom jsonlite fromJSON
-#' #'
-#' #' @examples
-#' #' \dontrun{
-#' #' labguru_get_element(element_id = 1)
-#' #' }
-#' labguru_get_element <- function(element_id,
-#'                                 server = Sys.getenv("LABGURU_SERVER"),
-#'                                 token  = Sys.getenv("LABGURU_TOKEN")) {
-#'   
-#'   # check arg element_id
-#'   
-#'   parsed <- labguru_get_by_id(type   = "element_containers",
-#'                               id     = element_id,
-#'                               server = server,
-#'                               token  = token)
-#'   
-#'   parsed
-#' }
-#' 
-#' 
-
-
-
-
+# #' Labguru get element
+# #'
+# #' Takes a element id and gets the element information.
+# #'
+# #' @param element_id numeric(1) id indicating a element on labguru server
+# #' @param server character(1) indicating the server URL
+# #' @param token character(1) access token for API authentication
+# #'
+# #' @return list object of labguru element
+# #' @export
+# #'
+# #' @import httr
+# #' @importFrom jsonlite fromJSON
+# #'
+# #' @examples
+# #' \dontrun{
+# #' labguru_get_element(element_id = 1)
+# #' }
+# labguru_get_element <- function(element_id,
+#                                 server = Sys.getenv("LABGURU_SERVER"),
+#                                 token  = Sys.getenv("LABGURU_TOKEN")) {
+#   
+#   # check arg element_id
+#   
+#   parsed <- labguru_get_by_id(type   = "element_containers",
+#                               id     = element_id,
+#                               server = server,
+#                               token  = token)
+#   
+#   parsed
+# }
+# 
+# 
