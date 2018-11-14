30a84772b98bfbaae5128613299a6f11eb108384
Jonathan Gross <jonathan.gross@biodata.com>
2018-11-14 10:19:52 +0100

    removing browser from uploadfile

--- R/upload_file.R
+++ R/upload_file.R
@@ -1,6 +1,6 @@
 
 #' Labguru upload file
-#' 
+#'
 #' Upload a file to Labguru
 #'
 #' @param file character(1) that is the path to a file
@@ -12,72 +12,72 @@
 #'
 #' @return list containing uploaded id and url
 #' @export
-#' 
+#'
 #' @import httr
 #' @importFrom jsonlite fromJSON
 #'
 #' @examples
 #' \dontrun{
 #' data("compensation")
-#' 
+#'
 #' library(ggplot2)
 #' plot <- ggplot(compensation,aes(x = Root, y = Fruit, colour = Grazing)) +
 #'   geom_point(size = 5) +
 #'   xlab("Root Biomass") +
 #'   ylab("Fruit Production") +
 #'   theme_bw()
-#'   
+#'
 #' ggsave("grazing.png", plot)
-#' 
+#'
 #' labguru_upload_image(file_path   = "grazing.png",
 #'                      name        = "Grazing",
 #'                      description = "Fruit production versus Root biomass for grazed and ungrazed")
 #' }
-labguru_upload_file <- function(file, 
+labguru_upload_file <- function(file,
                                 title,
                                 description    = NULL,
                                 # attach_to_uuid = NULL,
-                                server         = Sys.getenv("LABGURU_SERVER"), 
+                                server         = Sys.getenv("LABGURU_SERVER"),
                                 token          = Sys.getenv("LABGURU_TOKEN")) {
-  
+
   # Test arguments
-  check_arg_file(file)  
+  check_arg_file(file)
   check_arg_single_character(title, null = FALSE)
   check_arg_single_character(description, null = TRUE)
   check_arg_server(server)
   check_arg_token(token)
-  
+
   # URL
   base_url <- server
   path     <- "/api/v1/attachments"
-  
-  url <- httr::modify_url(url   = base_url, 
+
+  url <- httr::modify_url(url   = base_url,
                           path  = path)
-  
+
   # Body
   body <- list("token"                = token,
-               "item[title]"          = title, 
+               "item[title]"          = title,
                "item[description]"    = description,
                "item[attachment]"     = httr::upload_file(file))#,
-               # "item[attach_to_uuid]" = attach_to_uuid) 
-  
+               # "item[attach_to_uuid]" = attach_to_uuid)
+
   # Post
-  resp <- httr::POST(url    = url, 
+  resp <- httr::POST(url    = url,
                      body   = body)
-  
-  # Expect resp to be JSON 
+
+  # Expect resp to be JSON
   if (httr::http_type(resp) != "application/json") {
     stop("API did not return JSON", call. = FALSE)
   }
-  
+
   # Parse without simplifaction for consistency
   parsed <- jsonlite::fromJSON(httr::content(resp, as = "text"), simplifyVector = FALSE)
-  
+
   # check for request error
   if (httr::http_error(resp)) {
     stop(sprintf("API request failed [%s]\n%s", parsed$status, parsed$error), call. = FALSE)
   }
-  browser()
+  #browser()
   # invisible(TRUE)
   # RETURN ID AND URL
   list(id  = parsed$id,
@@ -89,13 +89,13 @@ labguru_upload_file <- function(file,
 
 
 #' Labguru upload visualization
-#' 
+#'
 #' Upload a visualization to the Labguru server.
-#' 
+#'
 #' @param file character(1) that is a path to a file
 #' @param title character(1) that is the title for the uploaded file on Labguru
 #' @param dataset_id Numeric(1) to link the visualization to dataset(s) on Labguru
-#' @param name character(1) that is the name for visualisation link with database 
+#' @param name character(1) that is the name for visualisation link with database
 #' @param description character(1) to describe visualization on Labguru
 #' @param server character(1) indicating the server URL
 #' @param token character(1) access token for API authentication
@@ -104,24 +104,24 @@ labguru_upload_file <- function(file,
 #' @export
 #'
 #' @examples
-labguru_upload_visualization <- function(file, 
+labguru_upload_visualization <- function(file,
                                          title,
                                          dataset_id     = NULL,
                                          name           = NULL,
                                          description    = NULL,
                                          # attach_to_uuid = NULL,
-                                         server         = Sys.getenv("LABGURU_SERVER"), 
+                                         server         = Sys.getenv("LABGURU_SERVER"),
                                          token          = Sys.getenv("LABGURU_TOKEN")) {
-  
+
   # Check arguments
-  check_arg_file(file)  
+  check_arg_file(file)
   check_arg_single_character(title, null = FALSE)
   check_arg_single_integer(dataset_id, null = FALSE)
   check_arg_single_character(name, null = TRUE)
   check_arg_single_character(description, null = TRUE)
   check_arg_server(server)
   check_arg_token(token)
-  
+
   # Upload file
   uploaded_file <- labguru_upload_file(file           = file,
                                        title          = title,
@@ -129,13 +129,13 @@ labguru_upload_visualization <- function(file,
                                        # attach_to_uuid = attach_to_uuid,
                                        server         = server,
                                        token          = token)
-  
+
   # Link file to dataset if dataset_id is set
   if (!is.null(dataset_id)) {
     # Check arguments
     # check_arg_dataset_id(dataset_id)
     # check_arg_name(name)
-    
+
     link <- labguru_link_visualization(dataset_id    = dataset_id,
                                        attachment_id = uploaded_file$id,
                                        name          = name,
@@ -152,7 +152,7 @@ labguru_upload_visualization <- function(file,
 
 
 #' Labguru link visualization
-#' 
+#'
 #' Link a visualization to a dataset on the Labguru server.
 #'
 #' @param dataset_id numeric(1) The dataset id for which to link visualization
@@ -163,18 +163,18 @@ labguru_upload_visualization <- function(file,
 #' @param token character(1) access token for API authentication
 #'
 #' @return
-#' 
+#'
 #' @import httr
 #' @importFrom jsonlite fromJSON
 #'
 #' @examples
-labguru_link_visualization <- function(dataset_id, 
+labguru_link_visualization <- function(dataset_id,
                                        attachment_id,
                                        name,
                                        description    = NULL,
-                                       server         = Sys.getenv("LABGURU_SERVER"), 
+                                       server         = Sys.getenv("LABGURU_SERVER"),
                                        token          = Sys.getenv("LABGURU_TOKEN")) {
-  
+
   # Test arguments
   check_arg_single_integer(dataset_id, null = FALSE)
   check_arg_single_integer(attachment_id, null = FALSE)
@@ -186,11 +186,11 @@ labguru_link_visualization <- function(dataset_id,
   # URL
   base_url <- server
   path     <- "/api/v1/visualizations"
-  
+
   # # Preferred
-  # url <- httr::modify_url(url   = base_url, 
+  # url <- httr::modify_url(url   = base_url,
   #                         path  = path)
-  # 
+  #
   # # Body
   # body <- list("token"               = token,
   #              "item[dataset_id]"    = dataset_id,
@@ -198,37 +198,37 @@ labguru_link_visualization <- function(dataset_id,
   #              "item[name]"          = name,
   #              "item[description]"   = description)
   # # Post
-  # resp <- httr::POST(url    = url, 
+  # resp <- httr::POST(url    = url,
   #                    body   = body,
   #                    encode = "json")
-  
+
   url <- paste(base_url, "/api/v1/visualizations", "?token=", token, sep="")
-  
-  data <- list(dataset_id    = dataset_id, 
-               attachment_id = attachment_id, 
-               name          = name, 
+
+  data <- list(dataset_id    = dataset_id,
+               attachment_id = attachment_id,
+               name          = name,
                description   = description)
-  
+
   item_to_post <- list(item = data)
-  
-  resp <- httr::POST(url, 
+
+  resp <- httr::POST(url,
                      body = item_to_post,
                      encode = "json")
-  
-  
-  # Expect resp to be JSON 
+
+
+  # Expect resp to be JSON
   if (httr::http_type(resp) != "application/json") {
     stop("API did not return JSON", call. = FALSE)
   }
-  
+
   # Parse without simplifaction for consistency
   parsed <- jsonlite::fromJSON(httr::content(resp, as = "text"), simplifyVector = FALSE)
-  
+
   # check for request error
   if (httr::http_error(resp)) {
     stop(sprintf("API request failed [%s]\n%s", parsed$status, parsed$error), call. = FALSE)
   }
-  
+
   # invisible(TRUE)
   # RETURN ID AND URL
   list(id  = parsed$id,
